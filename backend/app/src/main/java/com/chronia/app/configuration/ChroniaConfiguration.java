4379d6d00a809c8ba398a711f6ca7f1a6f21fe05
woowenjun99 <woowenjun99@gmail.com>
2025-07-06 16:59:18 +0800

    Initialise frontend

---
+++ backend/app/src/main/java/com/chronia/app/configuration/ChroniaConfiguration.java
@@ -0,0 +1,47 @@
+package com.chronia.app.configuration;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class ChroniaConfiguration {
+    @Value("${baseUrl}")
+    private String baseUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins(baseUrl)
+                        .allowedMethods("POST", "OPTIONS", "GET", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        return sessionFactory.getObject();
+    }
+}
